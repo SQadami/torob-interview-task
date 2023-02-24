feb7e80ef449550f4aba940508fe102c95d6a53f
Salman <salmanqadami@gmail.com>
2023-02-25 00:33:17 +0330

    feat: remove navGraph programatically init, change theme onCreate HomeActivity

--- app/src/main/java/ir/torob/sample/ui/HomeActivity.kt
+++ app/src/main/java/ir/torob/sample/ui/HomeActivity.kt
@@ -1,8 +1,8 @@
 package ir.torob.sample.ui
 
+import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ir.torob.sample.R
 import ir.torob.sample.databinding.ActivityHomeBinding
@@ -11,28 +11,15 @@ import ir.torob.ui.binding.BindingActivity
 @AndroidEntryPoint
 class HomeActivity : BindingActivity<ActivityHomeBinding>() {
 
-    private val navController by lazy { bindNavGraph() }
-
     override val bindingInflater: (LayoutInflater) -> ActivityHomeBinding
         get() = ActivityHomeBinding::inflate
 
     /**
      * enables click listener on the back button
      */
-    override fun onSupportNavigateUp() = navController.navigateUp()
-
-    // Configure the navigation
-    private fun bindNavGraph(): NavController {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val inflater = navHostFragment.navController.navInflater
-        val graph = inflater.inflate(R.navigation.nav_graph_home)
-
-        graph.setStartDestination(R.id.product_fragment)
-
-        val navController = navHostFragment.navController
-        navController.setGraph(graph, intent.extras)
-
-        return navController
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(ir.torob.ui.R.style.AppTheme)
+        super.onCreate(savedInstanceState)
     }
 }
