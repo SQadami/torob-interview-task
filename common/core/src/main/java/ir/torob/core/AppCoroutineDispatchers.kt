af0fe580d188e77d6387f9170cb4dd802bdbafb2
Salman <salmanqadami@gmail.com>
2023-02-24 03:34:58 +0330

    feat: add coroutine dispaters to application module di

---
+++ common/core/src/main/java/ir/torob/core/AppCoroutineDispatchers.kt
@@ -0,0 +1,9 @@
+package ir.torob.core
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+data class AppCoroutineDispatchers(
+    val io: CoroutineDispatcher,
+    val computation: CoroutineDispatcher,
+    val main: CoroutineDispatcher,
+)
